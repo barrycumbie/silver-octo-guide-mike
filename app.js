016f38bd74587698ada00e0a96a7ec995cfe9f53
Barry <barrycumbie@gmail.com>
2023-01-18 15:17:27 -0600

    refactor port env

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000; 
+
 
 app.get('/', (req, res) => {
   res.send('Hello World This is Barry 2!')
@@ -7,7 +9,9 @@ app.get('/', (req, res) => {
 
 console.log('in the node console');
 
-app.listen(process.env.PORT || 3000, () => console.log(`Server is running on PORT: ` ${process.env.PORT}));
+app.listen(PORT, () => {
+    console.log(`Server is running & listening on port: ${PORT}`);
+  });
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
