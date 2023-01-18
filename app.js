e03284f3817e1e89459073b36a16812a77f79c1a
Barry <barrycumbie@gmail.com>
2023-01-18 15:15:49 -0600

    ready for deploy

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World This is Barry 2!')
@@ -8,6 +7,8 @@ app.get('/', (req, res) => {
 
 console.log('in the node console');
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(process.env.PORT || 3000, () => console.log(`Server is running on PORT: ` ${process.env.PORT}));
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
